fc6470c2ce394ce95187d905eb2547ddc71826e6
JLA23 <eric.lefebvre59@orange.fr>
2015-10-14 11:02:02 +0200

    5.2

--- webserver/main.c
+++ webserver/main.c
@@ -9,6 +9,10 @@
 #include <stdlib.h>
 #include "socket.h"
 
+void error404(FILE *fichier){
+  fprintf(fichier,"%s", "HTTP/1.1 400 Bad Request\nConnection: close \nContent-Length: 17 \n\n400 Bad Request");
+}
+
 int main(){
   int socket_client;
   int socket_serveur = creer_serveur(8080);
@@ -25,27 +29,40 @@ int main(){
 	fichier = fdopen(socket_client, "w+");
 	sleep(3);
         if(write(socket_client, message_bienvenue, strlen(message_bienvenue)) == -1)
-         {
+	  {
 	  perror("Error Write Welcome");
-		/* traitement d’erreur */
-	 }
+	  /* traitement d’erreur */
+	  }
 	if(fgets(temp, 256, fichier) != NULL){
+	  fprintf(stderr, "ligne %s", temp);
 	  message = strdup(temp);
 	  type = strtok(message, " ");
 	  slash = strtok(NULL, " ");
 	  protocol = strtok(NULL, " ");
-	  if(strcmp(type, "GET") == 0 && strcmp(slash, "/") == 0 && (strncmp(protocol, "HTTP/1.1", 8) == 0 || strncmp(protocol, "HTTP/1.0", 8) == 0)){
-	    while(fgets(temp, 256, fichier) != NULL && (temp[0] != '\n' || temp[0] != '\r')){
-		printf("%s %s", "<Pawnee>", temp);
-	      }
-	    }
+	  if(strcmp(type, "GET") == 0 && (strncmp(protocol, "HTTP/1.1", 8) == 0 || strncmp(protocol, "HTTP/1.0", 8) == 0)){
+	    printf("%s %s %s", type, slash, protocol);
+	     while(fgets(temp, 256, fichier) != NULL && (temp[0] != '\n' || temp[0] != '\r')){
+	       fprintf(stderr, "DEBUG [%s]" ,temp);
+	       fflush(stdout);
+	     }
+	     fprintf(stderr, "DEBBUG2[%s]", temp);
+	     if(temp[0] == '\n' || temp[0] == '\r'){
+	       fprintf(stderr, "Ici \n");
+	       error404(fichier);
+	     }
+	  }
+	   else{
+	     	       fprintf(stderr, "la \n");
+	     error404(fichier);
+	   }
 	}
-  /* Utilisation methode accept et message de bienvenue */
+	
+	/* Utilisation methode accept et message de bienvenue */
 	fclose(fichier);
       }   
-    else {
-      close(socket_client);
-     }
+      else {
+	close(socket_client);
+      }
     } 
- return 1;	
+  return 1;	
 }
