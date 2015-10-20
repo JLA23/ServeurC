684ff105b29d32d09791f8899a53e6c18719132a
JLA23 <eric.lefebvre59@orange.fr>
2015-10-20 15:36:22 +0200

    error

--- webserver/main.c
+++ webserver/main.c
@@ -18,39 +18,33 @@ int main(){
   char *type, *slash, *protocol, *message;
   int pid;
   FILE * fichier;
-  const char * error404 = "HTTP/1.1 400 Bad Request\nConnection: close \nContent-Length: 17 \n\n400 Bad Request";
+  const char * error404 = "HTTP/1.1 400 Bad Request\nConnection: close \nContent-Length: 17 \n\n400 Bad Request\n";
   while((socket_client = accept(socket_serveur, NULL, NULL)) != -1)
     {
       pid = fork();
       if(pid == 0){
-	//sleep(3);
         if(write(socket_client, message_bienvenue, strlen(message_bienvenue)) == -1) {
 	  perror("Error Write Welcome");
 	  /* traitement d’erreur */
 	}
 	fichier = fdopen(socket_client, "w+");
-        fprintf(fichier, "test\n");
-	if(fgets(temp, 2048, fichier) != NULL){
-	  fprintf(stderr, "ligne %s", temp);
+	while(fgets(temp, 2048, fichier) != NULL && (temp[0] != '\n' || temp[0] != '\r')){
 	  message = strdup(temp);
 	  type = strtok(message, " ");
 	  slash = strtok(NULL, " ");
 	  protocol = strtok(NULL, " ");
 	  if(strcmp(type, "GET") == 0 && (strncmp(protocol, "HTTP/1.1", 8) == 0 || strncmp(protocol, "HTTP/1.0", 8) == 0)){
 	    printf("%s %s %s", type, slash, protocol);
-	    while(fgets(temp, 2048, fichier) != NULL && (temp[0] != '\n' || temp[0] != '\r')){
-	      // fprintf(stderr, "DEBUG [%s]" ,temp);
-	      
+	    while(fgets(temp, 2048, fichier) != NULL){
+	      fprintf(stderr, "%s" ,temp);
 	    }
-	    // fprintf(stderr, "DEBBUG2[%s]", temp);
+	    fprintf(fichier, "sortie");
 	    if(temp[0] == '\n' || temp[0] == '\r'){
-	       // fprintf(stderr, "Ici \n");
 	      fprintf(fichier, "%s", error404);
 	    }
 	  }
 	  else{
-	    fprintf(fichier, "test error\n");
-	    printf("%s",error404);
+	    fprintf(fichier, error404);
 	  }
 	}
 	
