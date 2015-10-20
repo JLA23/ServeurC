6c85b9d117464f1e8d3c1d8186429eaaf943a413
JLA23 <eric.lefebvre59@orange.fr>
2015-10-20 18:52:47 +0200

    chap 7

--- webserver/main.c
+++ webserver/main.c
@@ -10,62 +10,40 @@
 #include "socket.h"
 
 int main(){
+   const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
   int socket_client;
   int socket_serveur = creer_serveur(8080);
-  const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
-  int longeur = strlen(message_bienvenue);
   char temp[2048];
-  char *type, *slash, *protocol, *message;
   int pid;
+  int req = 0;
   FILE * fichier;
-  const char * error400 = "HTTP/1.1 400 Bad Request\nConnection: close \nContent-Length: 17 \n\n400 Bad Request\n";
-  const char * ok200 = "HTTP/1.1 200 OK\nConnection: close\nContent-length: %d\n\n";
-  const char * error404 = "HTTP/1.1 404 Bad Request\nConnection: close \nContent-Length: 17 \n\n404 Bad Request\n";
-  while((socket_client = accept(socket_serveur, NULL, NULL)) != -1)
-    {
-      pid = fork();
-      if(pid == 0){	
-	fichier = fdopen(socket_client, "w+");
-	if(fgets(temp, 2048, fichier) != NULL){
-	  message = strdup(temp);
-	  type = strtok(message, " ");
-	  slash = strtok(NULL, " ");
-	  protocol = strtok(NULL, " ");
-	  if(strcmp(type, "GET") == 0 && (strncmp(protocol, "HTTP/1.1", 8) == 0 || strncmp(protocol, "HTTP/1.0", 8) == 0)){
-	    printf("%s %s %s\n", type, slash, protocol);
-	    while(fgets(temp, 2048, fichier) != NULL ){
-	      if(temp[0] == '\n' || temp[strlen(temp)-1] != '\n'){
-		break;
-	      }
-	      else if(temp[0] == '\r'){
-		break;
-	      }
-	      else{
-	      fprintf(stderr, "%s" ,temp);
-	      }
-	    }
- 	    if(temp[strlen(temp)-1] != '\n'){
-	      fprintf(fichier, "%s", error400);
-	    }
-	    else if(strcmp(slash, "/") == 0){
-	      fprintf(fichier, ok200, longeur);
-	      fprintf(fichier, message_bienvenue);
-	    }
-	    else if(strcmp(slash, "/") != 0){
-	      fprintf(fichier, error404);
-	    }
-	  }
-	  else{
-	    fprintf(fichier, error400);
-	  }
-	}
-	
-	/* Utilisation methode accept et message de bienvenue */
-	fclose(fichier);
-      }   
-      else {
-	close(socket_client);
+  http_request requete;
+  while((socket_client = accept(socket_serveur, NULL, NULL)) != -1){
+    pid = fork();
+    if(pid == 0){	
+      fichier = fdopen(socket_client, "w+");
+      fgets_or_exit(temp,sizeof(temp),fichier);
+      req = parse_http_request(temp, &requete);
+      skip_headers(fichier);
+      if (!req){
+	send_response(fichier, 400, "Bad Request", "400 : Bad Request");
       }
-    } 
+      else if(requete.method == HTTP_UNSUPPORTED){
+	send_response(fichier, 405, "Method Not Allowed", "405 : Method Not Allowed");
+      }
+      else if(strcmp(requete.url, "/") == 0){
+	send_response(fichier, 200, "OK", message_bienvenue);
+      }
+      else{
+	send_response(fichier, 404, "Not Found", "404 : Not Found");
+      }
+      fclose(fichier);			
+    }
+    
+    /* Utilisation methode accept et message de bienvenue */
+    else {
+      close(socket_client);
+    }
+  } 
   return 1;	
 }
--- webserver/socket.h
+++ webserver/socket.h
@@ -4,6 +4,23 @@
 
 La fonction retourne -1 en cas d'erreur ou le descripteur de la socket créée. */
 
+enum http_method{
+  HTTP_GET,
+  HTTP_UNSUPPORTED,
+};
+
+typedef struct{
+  enum http_method method;
+  int major_version;
+  int minor_version;
+  char *url;
+} http_request;
+
 int creer_serveur(int port);
 void initialiser_signaux();
+int parse_http_request(const char *request_line , http_request *request);
+char * fgets_or_exit(char *buffer , int size , FILE *stream);
+void skip_headers(FILE *client);
+void send_status(FILE *client , int code , const char *reason_phrase);
+void send_response(FILE *client , int code , const  char *reason_phrase , const  char *message_body);
 #endif
