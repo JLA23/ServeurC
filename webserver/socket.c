aed033f463c6518ce43ecdabfebee4adc10a89fe
JLA23 <eric.lefebvre59@orange.fr>
2015-10-23 19:16:57 +0200

    url_or_404_clean

--- webserver/socket.c
+++ webserver/socket.c
@@ -12,7 +12,7 @@
 #include <stdlib.h>
 
 int creer_serveur(int port){
-
+  
   int socket_serveur;
   struct sockaddr_in saddr;
   saddr.sin_family = AF_INET; /* Socket ipv4 */
@@ -20,7 +20,7 @@ int creer_serveur(int port){
   saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
   int optval = 1;
   initialiser_signaux();
-
+  
   socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
   if (socket_serveur == -1)
     {
@@ -28,68 +28,68 @@ int creer_serveur(int port){
       /* traitement de l'erreur */
     }
   /* Utilisation de la socket serveur */
-
-   if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1)
-	{
-	perror("Can not set SO_REUSADDR option");
-	/*traitement d'erreur*/
-	}
-     /*Activation de l'option SO_REUSEADDR*/  
-
+  
+  if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1)
+    {
+      perror("Can not set SO_REUSADDR option");
+      /*traitement d'erreur*/
+    }
+  /*Activation de l'option SO_REUSEADDR*/  
+  
   if (bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1)
     {
       perror("bind socker_serveur");
       /* traitement d'erreur */
     }
   /* Utilisation de la methode bind */
-
+  
   if (listen(socket_serveur, 10) == -1)
     {
       perror("listen socket_serveur");
       /* traitement d'erreur */
     }
   /* Utilisation de la methode */
-
+  
   return socket_serveur;
 }
 
 int parse_http_request(const char *request_line , http_request *request){
-	char* met;
-	char* url;
-	char* http_version;
-	char* str = strdup(request_line);
-	if((met = strtok (str," ")) == NULL) return 0;
-	if((url = strtok (NULL, " ")) == NULL) return 0;
-	if((http_version = strtok (NULL, " ")) == NULL) return 0;
-	if(strcmp(met,"GET") == 0)
-		request->method = HTTP_GET;
-	else
-		request->method = HTTP_UNSUPPORTED;
-	if ((strcmp(http_version,"HTTP/1.0")==1) && (strcmp(http_version,"HTTP/1.1")==1)) return 0;
-	request->url = url;
-	return 1;
+  char* met;
+  char* url;
+  char* http_version;
+  char* str = strdup(request_line);
+  if((met = strtok (str," ")) == NULL) return 0;
+  if((url = strtok (NULL, " ")) == NULL) return 0;
+  if((http_version = strtok (NULL, " ")) == NULL) return 0;
+  if(strcmp(met,"GET") == 0)
+    request->method = HTTP_GET;
+  else
+    request->method = HTTP_UNSUPPORTED;
+  if ((strcmp(http_version,"HTTP/1.0")==1) && (strcmp(http_version,"HTTP/1.1")==1)) return 0;
+  request->url = url;
+  return 1;
 }
 
 char * fgets_or_exit(char *buffer , int size , FILE *stream){
-	char *buff;	
-	if((buff = fgets(buffer, size, stream)) == NULL) {
-		exit(1);
-	}
-	return buff;
+  char *buff;	
+  if((buff = fgets(buffer, size, stream)) == NULL) {
+    exit(1);
+  }
+  return buff;
 }
 
 
 void skip_headers(FILE *client){
-	char buf[1024];
-	while(buf[0] != '\r' && buf[1] != '\n'){
-	  fgets_or_exit(buf, sizeof(buf), client);
-	}
+  char buf[1024];
+  while(buf[0] != '\r' && buf[1] != '\n'){
+    fgets_or_exit(buf, sizeof(buf), client);
+  }
 }
 
 void send_status(FILE *client , int code , const char *reason_phrase){
 	char status[256];
 	sprintf(status, "HTTP/1.1 %d %s\r\n", code, reason_phrase);
-	fprintf(client, status);
+	fprintf(client, "%s", status);
 }
 
 void send_response(FILE *client , int code , const  char *reason_phrase , const  char *message_body){
@@ -98,9 +98,9 @@ void send_response(FILE *client , int code , const  char *reason_phrase , const 
 		char content_length[256];
 		sprintf(content_length, "Content-Length: %zu\r\n", strlen(message_body));
 		fprintf(client, "Content-Type: text/html\r\n");
-		fprintf(client, content_length);
+		fprintf(client, "%s", content_length);
 		fprintf(client, "\r\n");
-		fprintf(client, message_body);
+		fprintf(client, "%s", message_body);
 	}
 	fprintf(client, "\r\n");
 }
